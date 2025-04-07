909a49ec0a3dadc6554080cd7997aab8e8fff554
NoahKlomp <164741222+NoahKlomp@users.noreply.github.com>
2025-04-07 20:12:31 +0000

    a lot

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -80,11 +80,11 @@ const config: Config = {
           label: 'Tutorial',
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
