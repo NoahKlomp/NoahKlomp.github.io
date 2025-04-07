0e515f7c36b771886863b411e5e02a92d586554c
NoahKlomp <164741222+NoahKlomp@users.noreply.github.com>
2025-04-07 19:58:46 +0000

    remove edit this page link

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -36,11 +36,10 @@ const config: Config = {
       'classic',
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: './sidebars.ts'
+          
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         // blog: {
         //   showReadingTime: true,
